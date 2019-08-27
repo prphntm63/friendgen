3a5a309109b0b843cfea1f0c7e712165bba11885
william <wjeffharris@outlook.com>
2019-08-27 17:03:49 -0500

    Pushing geolocation,returing lat et lon

---
+++ getLocation.js
@@ -0,0 +1,34 @@
+function locateMe(){
+    if('geolocation' in navigator){
+        console.log('geolocation is available')
+    
+        navigator.geolocation.getCurrentPosition(function(position){
+          
+            const lat = position.coords.latitude
+            const lon = position.coords.longitude
+            document.getElementById('latitude').textContent = lat
+            document.getElementById('longitude').textContent = lon
+    
+             console.log(position)
+
+            // const data = {lat, lon}
+            // const options = {
+            //     method: 'POST',
+            //     headers: {
+            //         'content-type': 'appication/json'
+            //     },
+            //     body: JSON.stringify(data)
+            // }
+            // fetch('/api', options)
+        })
+        } else {
+        console.log('geolocater is not available')
+        }
+        
+}
+// const express = require('express');
+// const app = express();
+// app.listen(3000, function(){console.log()}
+// app.post('/api', function(request, response){
+//     console.log(request);
+// });
