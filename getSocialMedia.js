44ba9b2bb0df144e08a96ca28b9f7650f0b6b690
Pia Soy <piasoy4@gmail.com>
2019-08-29 23:16:48 -0500

    create wrapper function getFacebookData() to return a promise

--- getSocialMedia.js
+++ getSocialMedia.js
@@ -1,46 +1,57 @@
-  window.fbAsyncInit = function() {
-          FB.init({                                     //initialize and setup the SDK  //FB object with capabilities fo fb API
-            appId            : '931960383805197',       //ID of Facebook App
-            autoLogAppEvents : true,
-            version          : 'v4.0',
-          });
-         
-        };   
+ //initialize and setup the SDK    
+ window.fbAsyncInit = function() {
+    FB.init({                                      
+      appId            : '931960383805197',      
+      autoLogAppEvents : true,
+      version          : 'v4.0',
+    });
+   
+}; 
 
 
-        function login() {                                 
-            FB.login(ifAuthorized);                  //FB.login returns a response which is then passed in as parameter to ifAuthorize                                        
-        } 
+function getFacebookData() {
+    
+    var promise = new Promise(function(resolve, reject) {
+    let dbUserStructure = {};
+    let userData={};
 
+
+        //authenticate user                          
+        FB.login(ifAuthorized);                                                        
+    
+        //handle user response
         function ifAuthorized(response){             //response is {name: "Pia Soy", id: "10156251899951436"}    
-            if (response.authResponse) {            //what is authResponse?
-                getUserData(response);              //call getUserData
+            if (response.authResponse) {           
+                getUserData(response);              
             } else {
-            console.log('User cancelled login or did not fully authorize.');
+                reject('User cancelled login or did not fully authorize.');
             }            
         }
-
-        let userData={};
-        function getUserData (){
-            FB.api(                                 //calls the Graph API
-                '/me',                              //user_id
-                'GET',                              //method to request data
+    
+        //call the Graph API
+        function getUserData () {
+            FB.api(                                
+                '/me',                             
+                'GET',                             
                 {"fields":"id,name,first_name,picture.type(large),likes{name,category}"},
-                function(response) {                   
-                    userData = response;
-                    console.log("userData: ",userData)
-                    getEncodedProfilePic(userData.picture.data.url)
-                    return userData;
-                }
-                );
+                handleUserData
+            );
         }
 
+        //handle FB user data
+        function handleUserData(response) {
+            userData = response;
+            getEncodedProfilePic(userData.picture.data.url)
+        }
+
+        //handle FB profile pic data
         function getEncodedProfilePic(downloadURL) {
+            var imageURL = "test"
             if (location.hostname === 'localhost') {
-                // downloadURL = 'https://cors-anywhere.herokuapp.com/' + downloadURL
+                downloadURL = 'https://cors-anywhere.herokuapp.com/' + downloadURL
             }
             var xmlHTTP = new XMLHttpRequest();
-            xmlHTTP.open('GET',downloadURL,true);
+            xmlHTTP.open('GET',downloadURL,true)
             xmlHTTP.responseType = 'arraybuffer';
             xmlHTTP.onload = function(e) {
                 var arr = new Uint8Array(this.response)
@@ -48,8 +59,47 @@
                 var b64 = btoa(raw)
                 var dataURL='data:image/jpeg;base64,'+b64
                 document.getElementById('testImage').src=dataURL;
-                console.log(this.response)
-                return dataURL // Use this like <img src="${getEncodedProfilePic(downloadURL)"}>
+    
+                createDBUserStructure(dataURL);
+
             }
             xmlHTTP.send();
+        
         }
+
+        //create database structure from FB user data
+        function createDBUserStructure (dataURL) {
+            let likeNames =[];
+            let likeCategories =[];
+
+            //loop through userData.likes.data array, push every fb page liked
+            userData.likes.data.forEach(function(likeItem){likeNames.push(likeItem.name)});
+            //loop through userData.likes.data array, push every category of fb page liked
+            userData.likes.data.forEach(function(likeItem){likeCategories.push(likeItem.category)});
+            //remove duplicate categories
+            let filteredLikeCategories = likeCategories.filter(function (item, index){
+                return likeCategories.indexOf(item) === index;            
+            })
+
+            //create structure object
+            dbUserStructure["id"] = userData.id;
+            dbUserStructure["name"] = userData.first_name;
+            dbUserStructure["likes"] = likeNames
+            dbUserStructure["categories"] = filteredLikeCategories;      
+            dbUserStructure["dataURL"] = dataURL
+            
+            resolve(dbUserStructure)
+            console.log(promise1)
+
+        }
+
+
+    });
+
+    //emulate next function
+    promise.then(function(dbValue) {
+        console.log("dbValue", dbValue);
+    
+    });
+}
+
